b292c7644b7ff279ca778955eade0ecfde14085e
Bhat Owais <owaisbhat996@outlook.com>
2023-04-27 10:31:31 +0530

    Updated UI

--- components/StatsResult.js
+++ components/StatsResult.js
@@ -13,10 +13,13 @@ export default function StatsResult({ data, filter }) {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-3 py-3">
+                S No.
+              </th>
+              <th scope="col" className="px-3 py-3">
                 Player name
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-3 py-3">
                 Count
               </th>
             </tr>
@@ -29,6 +32,9 @@ export default function StatsResult({ data, filter }) {
                     key={index}
                     className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                   >
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {index+1}
+                    </th>
                     <th
                       scope="row"
                       className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
